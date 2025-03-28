1cecf4c497851d70ef48fb0a9e4a998ba040efb9
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-03-29 00:52:17 +0530

    initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import ReactDOM from "react-dom/client"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  createRoutesFromElements,
-  
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Board from "./pages/Board.jsx";
 
 
 function App() {
-  
+
+ const Home=()=><h1>home page</h1>
   
 
 
   return (
     <>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/board">Board</Link></li>
+          <li><Link to="/">Home</Link></li>
+         
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/board" element={<Board/>} />
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
     
      
     </>
